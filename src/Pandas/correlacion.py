99458eb85b0600bf35c8801b80d50a9ae5136240
Manuel A. Lazo (Desktop) <manuel.antonio.lazo@gmail.com>
2025-08-28 10:31:46 -0500

    Nivelacion trabajo riesgos

--- src/Pandas/correlacion.py
+++ src/Pandas/correlacion.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from statsmodels.tsa.stattools import adfuller
 
 # Cargar datos (puede ser Excel o CSV)
 df = pd.read_csv("src/Pandas/datos.csv",sep=";")
@@ -7,16 +8,21 @@ df = pd.read_csv("src/Pandas/datos.csv",sep=";")
 for col in df.columns:
     df[col] = pd.to_numeric(df[col], errors="coerce") 
 
+result = adfuller(df["au"].dropna())  # dropna para evitar NaN
+print("ADF Statistic:", result[0])
+print("p-value:", result[1])
+print("Critical Values:", result[4])
+
 # Matriz de correlación
-correlation_matrix = df.corr()
+# correlation_matrix = df.corr()
 
 # Matriz corrleación anticipada
-corr_anticipadas = df.corrwith(df.shift(-1)["pendiente"])
+# corr_anticipadas = df.corrwith(df.shift(-1)["pendiente"])
 
 # Matriz corrleación rezagada
-corr_rezagada = df.corrwith(df.shift(+1)["pendiente"])
+# corr_rezagada = df.corrwith(df.shift(+1)["pendiente"])
 
-print(correlation_matrix)
+# print(correlation_matrix)
 
-print(corr_anticipadas)
-print(corr_rezagada)
+# print(corr_anticipadas)
+# print(corr_rezagada)
